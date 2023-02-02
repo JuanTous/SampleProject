22de70b669fbfff1ccb51d272c69ff577cb8fc8f
Juan Tous <tousjuan01@gmail.com>
2023-02-02 18:12:14 -0500

    Add StyleSheet

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import LittleLemonFooter from './components/LittleLemonFooter';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
-import LittleLemonFooter from './components/LittleLemonFooter';
-import WelcomeScreen from './components/WelcomeScreen';
+import MenuItems from './components/MenuItems';
+
 export default function App() {
   return (
     <>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: '#495E57',
-        }}>
+      <View style={styles.container}>
         <LittleLemonHeader />
-        <WelcomeScreen />
+        <MenuItems />
       </View>
-      <View style={{ backgroundColor: '#495E57' }}>
+      <View>
         <LittleLemonFooter />
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#495E57',
+  },
+});
--- components/LittleLemonFooter.js
+++ components/LittleLemonFooter.js
@@ -1,21 +1,26 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function LittleLemonFooter() {
   return (
     <View
-      style={{
-        backgroundColor: '#F4CE14',
-        marginBottom: 10,
-      }}>
+      style={footerStyles.container}>
       <Text
-        style={{
-          fontSize: 18,
-          color: 'black',
-          textAlign: 'center',
-        }}>
+        style={footerStyles.innerText}>
         All rights reserved by Little Lemon, 2022{' '}
       </Text>
     </View>
   );
 }
+
+const footerStyles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F4CE14',
+    marginBottom: 10,
+  },
+  innerText: {
+    fontSize: 18,
+    color: 'black',
+    textAlign: 'center',
+  }
+});
