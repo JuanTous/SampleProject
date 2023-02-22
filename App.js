21d00d8f4830c9bc5b5fc71093ab2308b7697a0f
Juan Tous <tousjuan01@gmail.com>
2023-02-22 17:42:29 -0500

    Image component

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { View, Text, StyleSheet } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import LoginScreen from './LoginScreen';
+import Welcome from './WelcomeScreen';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <LoginScreen />
+        <Welcome />
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
