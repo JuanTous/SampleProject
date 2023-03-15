a63b7370d001e425ac3c8014a30508e83996d273
Juan Tous <tousjuan01@gmail.com>
2023-03-15 17:25:21 -0500

    useWindowDimensions

--- WelcomeScreen.js
+++ WelcomeScreen.js
@@ -3,21 +3,13 @@ import {
   Image,
   StyleSheet,
   Text,
-  useColorScheme,
+  useWindowDimensions,
 } from 'react-native';
 
-const WelcomeScreen = () => {
-  const colorScheme = useColorScheme();
-
+const Welcome = () => {
+  const window = useWindowDimensions();
   return (
-    <ScrollView
-      style={[
-        styles.container,
-        colorScheme === 'light'
-          ? { backgroundColor: '#fff' }
-          : { backgroundColor: '#333333' },
-      ]}>
-           {' '}
+    <ScrollView style={styles.container}>
       <Image
         style={styles.logo}
         source={require('./img/littleLemonLogo.png')}
@@ -25,7 +17,13 @@ const WelcomeScreen = () => {
         accessible={true}
         accessibilityLabel={'Little Lemon Logo'}
       />
-          <Text style={styles.regular}>Color Scheme: {colorScheme}</Text>   {' '}
+      <Text style={styles.title}>
+        Little Lemon, your local Mediterranean Bistro
+      </Text>
+      <Text style={styles.regular}>Window Dimensions</Text>
+      <Text style={styles.regular}>Height: {window.height}</Text>
+      <Text style={styles.regular}>Width: {window.width}</Text>
+      <Text style={styles.regular}>Font scale: {window.fontScale}</Text>
     </ScrollView>
   );
 };
@@ -34,16 +32,23 @@ const styles = StyleSheet.create({
   logo: {
     height: 100,
     width: 300,
+    resizeMode: 'contain',
   },
   container: {
     flex: 1,
     padding: 24,
     marginTop: 25,
+    backgroundColor: '#fff',
   },
-  regular: {
-    fontSize: 18,
+
+  title: {
+    marginTop: 16,
+    paddingVertical: 10,
+    color: '#333333',
     textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
-export default WelcomeScreen;
+export default Welcome;
