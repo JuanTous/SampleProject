82cce648f2f836f9fb245ec9fc39e399b59ee9df
Juan Tous <tousjuan01@gmail.com>
2023-03-17 19:45:54 -0500

    React Navigation

--- App.js
+++ App.js
@@ -4,17 +4,27 @@ import { View, Text, StyleSheet } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './WelcomeScreen';
+import LoginScreen from './LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <View style={styles.container}>
-        <LittleLemonHeader />
-        <WelcomeScreen />
-      </View>
-      <View style={styles.footerContainer}>
-        <LittleLemonFooter />
-      </View>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
+      </NavigationContainer>
     </>
   );
 }
@@ -26,3 +36,4 @@ const styles = StyleSheet.create({
   },
   footerContainer: { backgroundColor: '#333333' },
 });
+
